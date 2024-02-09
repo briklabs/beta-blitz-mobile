530dbff939bff82439688a69c40c764e3e6b0769
krystlc <alex.valle@gmail.com>
2024-02-09 00:09:09 -0500

    feat(workout): added workout cta

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,18 +1,41 @@
 import React from "react";
-import { View, Button, Text, Pressable, ScrollView } from "react-native";
+import { View, Pressable, ScrollView, Text } from "react-native";
 import { ScreenProps } from "../utils/types";
-import { Icon } from "@rneui/themed";
+import { Avatar, Button, Icon } from "@rneui/themed";
+
+function WorkoutProgress() {
+  return (
+    <View className="flex-row justify-between items-center flex-1 p-4">
+      <View className="gap-2">
+        <Text className="text-white text-xl font-medium">Workout Progress</Text>
+        <Text className="text-neutral-400">12 Exercises left</Text>
+      </View>
+      <Avatar
+        size={60}
+        rounded
+        title="14"
+        containerStyle={{ borderColor: "#f44344", borderWidth: 4 }}
+      />
+    </View>
+  );
+}
 
 const HomeScreen = ({ navigation }: ScreenProps<"HomeScreen">) => {
   return (
     <View className="flex-1">
       <ScrollView>
-        <Button
-          title="Go to Workout Calculator"
-          onPress={() => navigation.navigate("WorkoutCalculator")}
-        />
+        <View className="p-4">
+          <Button
+            title={<WorkoutProgress />}
+            buttonStyle={{
+              borderRadius: 20,
+              backgroundColor: "#111",
+            }}
+            onPress={() => navigation.navigate("WorkoutCalculator")}
+          />
+        </View>
       </ScrollView>
-      <View className="flex-row py-8 justify-evenly gap-8 bg-white">
+      <View className="flex-row py-6 justify-evenly gap-8 bg-white">
         <Pressable>
           <Icon name="home" type="feather" />
         </Pressable>
