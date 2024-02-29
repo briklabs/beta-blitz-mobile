8cf920a7b47c7ee3072757736c7133d534ac9bb0
krystlc <alex.valle@gmail.com>
2024-02-29 00:58:21 -0500

    feat(frontpage): re-added home page

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,48 +1,32 @@
-import React from "react";
-import { View, ScrollView, Pressable } from "react-native";
+import React, { useState } from "react";
+import { View } from "react-native";
 import { ScreenProps } from "../utils/types";
-import BetaBlitz from "../features/beta-blitz/BetaBlitz";
-import { useBetaBlitzContext } from "../features/beta-blitz/BetaBlitzContext";
-import { Avatar, Icon, Text, useTheme } from "react-native-paper";
-
-function WorkoutProgress() {
-  const { total, goal } = useBetaBlitzContext();
-  const subtitle = total
-    ? `${goal - total} route points to go, let's go!`
-    : "Time attack route counter";
-
-  const theme = useTheme();
-  return (
-    <View
-      style={{
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        padding: 12,
-        borderWidth: 1,
-        borderRadius: theme.roundness,
-        backgroundColor: theme.colors.background,
-      }}
-    >
-      <View style={{ gap: 2 }}>
-        <Text variant="headlineSmall">Beta Blitz</Text>
-        <Text>{subtitle}</Text>
-      </View>
-      {total ? (
-        <Avatar.Text size={60} label={String(total)} />
-      ) : (
-        <Icon source="lightning-bolt" size={48} color={theme.colors.tertiary} />
-      )}
-    </View>
-  );
-}
+import { Button } from "react-native-paper";
 
 const HomeScreen = ({ navigation }: ScreenProps<"HomeScreen">) => {
   return (
-    <View>
-      <ScrollView>
-        <View style={{ padding: 4 }}></View>
-      </ScrollView>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        gap: 24,
+      }}
+    >
+      <Button
+        mode="contained"
+        onPress={() => navigation.push("BetaBlitzScreen")}
+        icon="lightning-bolt"
+      >
+        Start
+      </Button>
+      <Button
+        mode="text"
+        onPress={() => navigation.push("GettingStartedScreen")}
+        icon="flag"
+      >
+        Getting Started
+      </Button>
     </View>
   );
 };
