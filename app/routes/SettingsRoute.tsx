a5343b6edaf6dbbaa56a45291dcadd279e8db4ea
krystlc <alex.valle@gmail.com>
2024-03-10 03:20:54 -0400

    feat(settings): clear data

--- app/routes/SettingsRoute.tsx
+++ app/routes/SettingsRoute.tsx
@@ -1,11 +1,31 @@
 import React from "react";
 import { SafeAreaView } from "react-native";
-import { Text } from "react-native-paper";
+import { Button, Card, Text, useTheme } from "react-native-paper";
+import { dropTables } from "../db/beta-blitz.repo";
 
 export default function SettingsRoute() {
+  const theme = useTheme();
   return (
-    <SafeAreaView>
-      <Text>SettingsRoute</Text>
+    <SafeAreaView style={{ gap: 4 }}>
+      <Card>
+        <Card.Title title="Settings" />
+        <Card.Content>
+          <Text variant="bodyMedium">
+            In case you need to clean your history, use the button below to drop
+            all data.
+          </Text>
+        </Card.Content>
+        <Card.Actions>
+          <Button
+            mode="contained-tonal"
+            onPress={dropTables}
+            buttonColor={theme.colors.error}
+            textColor={theme.colors.errorContainer}
+          >
+            Clear workout sessions
+          </Button>
+        </Card.Actions>
+      </Card>
     </SafeAreaView>
   );
 }
